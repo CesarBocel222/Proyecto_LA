6ab7ba113dc73b10013363315c012c7d4a4e6af0
César <83521963+CesarBocel222@users.noreply.github.com>
2024-04-11 07:27:13 -0600

    Add files via upload

--- Proyecto Lenguajes Formales y Automatas/Program.cs
+++ Proyecto Lenguajes Formales y Automatas/Program.cs
@@ -1,4 +1,5 @@
-﻿using Proyecto_Lenguajes_Formales_y_Automatas.DATA;
+﻿using Proyecto_Lenguajes_Formales_y_Automatas.ARBOL;
+using Proyecto_Lenguajes_Formales_y_Automatas.DATA;
 using System;
 using System.IO;
 using System.Text.RegularExpressions;
@@ -6,91 +7,98 @@ using System.Text.RegularExpressions;
 class Program
 {
     //Se uso un metodo de github https://github.com/SantiagoBocel/Lenguajes para las verificaciones
+    //Archivos
+    // "C:/Users/50255/Desktop/URL/Septimo semestre/Lenguajes Formales y Automatas/Proyecto Lenguajes Formales y Automatas/Archivos/prueba3.txt";
+    //"C:/Users/50255/Desktop/URL/Septimo semestre/Lenguajes Formales y Automatas/Proyecto Lenguajes Formales y Automatas/Archivos/GRAMATICA.txt"
+    //"C:/Users/50255/Desktop/URL/Septimo semestre/Lenguajes Formales y Automatas/Proyecto Lenguajes Formales y Automatas/Archivos/GRAMATICA-3.txt"
+    //"C:/Users/50255/Desktop/URL/Septimo semestre/Lenguajes Formales y Automatas/Proyecto Lenguajes Formales y Automatas/Archivos/GRAMATICA-4.txt"
+    //"C:/Users/50255/Desktop/URL/Septimo semestre/Lenguajes Formales y Automatas/Proyecto Lenguajes Formales y Automatas/Archivos/GRAMATICA-5.txt"
+    //"C:/atchivo.txt"
     static void Main()
     {
-        Console.WriteLine("_______Bienvenido Parte 1: Lenguajes Formales y Automatas______\n");
-        string filePath = "C:/Users/50255/Desktop/URL/Septimo semestre/Lenguajes Formales y Automatas/Proyecto Lenguajes Formales y Automatas/Archivos/prueba3.txt";
+        var pathFile = "";
+        Console.WriteLine("_______Bienvenido al Generador de Scanner: Lenguajes Formales y Automatas______\n");
+        Console.WriteLine("Ingrese el path del archivo: ");
+        pathFile = Console.ReadLine();
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine("\n\t Espere");
         do
         {
+            ArbolExpresion expressionTree = new ArbolExpresion();
+            if (!Regex.IsMatch(pathFile, @".+\.txt")) pathFile += ".txt";
             var al = new Verificaror();
-            var gramatica = al.ReadFile(filePath);
+            var gramatica = al.ReadFile(pathFile);
             if (gramatica.Count > 0)
             {
                 var ordenCorrecto = al.VerifyOrderSection(gramatica);
                 if (ordenCorrecto)
                     if (al.VerifyGramar(gramatica))
-                        Console.WriteLine("Gramática correcta. ");
+                        Console.WriteLine("Gramática correcta. :P");
+                //Ceacion del AFD
+                if (al.getTokens() != null)
+                {
+                    ExpresionRegular parametros = new ExpresionRegular();
+                    parametros.ProcessRawLists(al.getTokens());
+                    expressionTree.CreateTree(parametros, al.getSets().Count);
+                    expressionTree.GenerateDFA();
+
+                    //  TODO: Pendiente
+                    expressionTree.LlenarEstadosAceptacion();
+                    expressionTree.LlenarMovimientosEstado();
+                    Console.WriteLine("---------------------------------------");
+                    Console.WriteLine("Ingrese el número indicado para mosrar la información o 0 para salir del menu.");
+                    var informacionGenerada = "";
+                    while (informacionGenerada != "0")
+                    {
+                        Console.Write("\nNúmero indicador - Información generada\n1 - Nodos del árblo\n2 - Árbol de expresiones\n3 - Tabla de follow\n4 - Tabla de transiciones\n");
+                        informacionGenerada = Console.ReadLine();
+                        Console.WriteLine("---------------------------------------");
+                        if (Regex.IsMatch(informacionGenerada, @"\d"))
+                        {
+                            switch (informacionGenerada)
+                            {
+                                case "0":
+                                    Console.WriteLine("Regresando.");
+                                    break;
+                                case "1":
+                                    expressionTree.printNodes();
+                                    break;
+                                case "2":
+                                    Console.WriteLine("--------VISTA EN UNA LINEA--------");
+                                    expressionTree.RecorridoInOrden();
+                                    Console.WriteLine("\n--------VISTA POR NODOS--------");
+                                    expressionTree.verArbol();
+                                    break;
+                                case "3":
+                                    expressionTree.printFollow();
+                                    break;
+                                case "4":
+                                    expressionTree.printDFATable();
+                                    break;
+                                default:
+                                    Console.WriteLine("Opción inválida.");
+                                    break;
+                            }
+                        }
+                        else Console.WriteLine("Opción no reconocida.");
+                        Console.WriteLine("\n---------------------------------------");
+                    }
+                }
+
+
+
                 Console.WriteLine("-------------------------------------------------------");
-                Console.WriteLine("\nIngrese q para salir o el nombre de un nuevo archivo");
-                filePath = Console.ReadLine();
-                Console.WriteLine("\n\t Espere...");
+                Console.WriteLine("\nIngrese q para salir o el path de un nuevo archivo");
+                pathFile = Console.ReadLine();
+                al.nullSets();
+                al.nullTokens();
+                Console.WriteLine("\n\t Espere... :)");
             }
             else
             {
                 Console.WriteLine("Ingrese el nombre de otro archivo");
-                filePath = Console.ReadLine();
+                pathFile = Console.ReadLine();
             }
-        } while (!Regex.IsMatch(filePath, @"[Qq]"));
-
-
-    }
-
-    static string checkPattern(List<string> list, string pattern, int fila)
-    {
-        string verificationString = ""; //Debe contener la informacion sobre la validación
-        if (list.Count > 1)//si existe almenos 1 elemento más que el indicador de la parte
-        {
-            int row = fila; //indica la fila actual
-            foreach (string element in list) //recorre la lista elemento por elemento
-            {
-                if (!Regex.IsMatch(element, pattern))//si el elemento no cumple con el patron
-                {
-                    //verificar que columna falla
-                }
-                row++;//aumenta el indicador de la fila actual
-            }
-            return verificationString;
-        }
-        else return "No hay suficientes elementos para verificar";
-
-    }
-    // Distribuidor de partes de la gramatica con su respectivo patron
-    // <param name="list">Recibe una lista que contiene la parte de la gramática que se verificará</param>
-    static void checksDealer(List<string> list)
-    {
-        string setsPattern = @"(\s*[A-Z]+ *= *)(('\w+'((\+|\.{2})?'\w+')*)\s*|(CHR\(\d+\)((\+|\.{2})?CHR\(\d+\))*)|\s)";
-        string tokensPattern = @"(TOKEN\s*\d+\s*=\s*)(('.{1}')+|(\w+ *(\*|\|)?)+|('.{1}' *\w+ *'.{1}'(\*|\|)?)+|(\w*\s*(\(|{)(\s*\w*\s*(\(\))?(\*|\|)?)+(\)|})(\*|\|)?)+)";
-        string actionsPattern = @"([A-Z0-9_]+\(\))|({\s*)|(}\s*)|(\s*\d*\s*=\s*'[A-Z]*'\s*)|\s*";
-        string errorsPattern = @"(\s*\w*ERROR\w*\s*=)(\s*\d*\s*)";
-        int actualRow = 0;
-        if (Regex.IsMatch(list[0], @"SETS\s*"))//Si el primer elemento de la lista es SETS 
-        {
-            var verificationResult = checkPattern(list, setsPattern, actualRow);
-            //verificationResult debe interpretarse para saber si hay un error si es asi detener la verificacion
-
-        }
-        else if (Regex.IsMatch(list[0], @"TOKENS\s*"))//Si el primer elemento de la lista es TOKENS
-        {
-            var verificationResult = checkPattern(list, tokensPattern, actualRow);
-        }
-        else if (Regex.IsMatch(list[0], @"ACTIONS\s*"))//Si el primer elemento de la lista es ACTIONS
-        {
-            foreach (string reservadasSerach in list)
-            {
-                if (Regex.IsMatch(reservadasSerach, @"\s*RESERVADAS.*"))//Verifica si existe la funcion RESERVADAS
-                {
-                    var verificationResult = checkPattern(list, actionsPattern, actualRow);
-                }
-                else
-                {
-                    Console.WriteLine("Falta la funcion reservadas no es una gramática válida");
-                }
-            }
-
-        }
-        else if (Regex.IsMatch(list[0], @"ERROR.*"))//Si el primer elemento de la lista es un ERROR
-        {
-            var verificationResult = checkPattern(list, errorsPattern, actualRow);
-        }
+        } while (!Regex.IsMatch(pathFile, @"[Qq]"));
     }
 }
